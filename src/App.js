d0b50a39253a6249d08ccd7ab2fbff78b7792027
Yoshito <yoshityan.ibatyan@gmail.com>
2021-07-19 22:47:16 +0900

    station5まで

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+// import * as React from 'react'
+
+import React, {useState} from 'react'
 import './App.css'
 
 /**
  * 
  * @type {React.FC}
  */
+
 export const App = () => {
+  const p = "犬の画像を表示するBBS";
+  // const setDogUrl = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/shiba/shiba-1.jpg');
+
   return (
     <div>
       <header>BBS</header>
-      <p>犬の画像を表示するBBS</p>
+      <p>{p}</p>
       <img
-              src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-              alt="dog"
-            />
+        src={dogUrl}
+        alt="dog"
+      />
+      <br/>
+      <button onClick={() => {setDogUrl('https://images.dog.ceo/breeds/shiba/shiba-8.jpg')}}>更新</button>
     </div>
-  )
+  );
 }
