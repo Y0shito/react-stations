55852db39b5f677867ffc92638f347051ebb8af9
Yoshito <yoshityan.ibatyan@gmail.com>
2021-09-03 09:59:16 +0900

    ドロップダウンリスト完成

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,14 @@
 // DO NOT DELETE
+import * as React from 'react'
+
+export const BreedsSelect = (props) => {
+  const values = props.values;
+
+  return (
+    <select name="breeds">
+      {values.map((value) =>
+        <option key={value} value={value}>{value}</option>
+      )}
+    </select>
+  )
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,17 +1,18 @@
 // DO NOT DELETE
 import React, { useState, useEffect } from 'react'
+import { BreedsSelect } from './BreedsSelect';
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState();
+  const [breeds, setBreeds] = useState([]);
 
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
       .then(data => {
-        setBreeds(data.message);
-        console.log(data.message);
+        setBreeds(Object.keys(data.message));
       });
   }, [])
 
-  return (<p>test</p>);
+  return (
+    <BreedsSelect values={breeds} />);
 }
